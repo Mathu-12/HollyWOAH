71bd9c001b7dd8041ff936b4c860f4d2c630af15
Mathu-12 <mathuthirupathi12@gmail.com>
2025-04-03 21:39:26 +0530

    Update auth.js

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -42,12 +42,6 @@ router.post("/login", async (req, res) => {
 
     const user = await User.findOne({ email });
     if (!user) return res.status(400).json({ message: "User not found" });
-
-    const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) return res.status(400).json({ message: "Invalid credentials" });
-
-    const token = jwt.sign({ id: user._id }, JWT_SECRET, { expiresIn: "1h" });
-
     res.json({ 
       message: "✅ Login successful",
       token, 
